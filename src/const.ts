dc42a8e1e1cdd8d8d8312c53c86051cc3521771f
Naoki <naoki.au.whm@gmail.com>
2023-06-08 21:31:09 +1000

    image optimization

--- src/const.ts
+++ src/const.ts
@@ -1,70 +1,70 @@
 export const MOST_VOTED = [
   {
     name: "飯田将成",
-    src: "./iida_masanari.png",
+    src: "https://res.cloudinary.com/deevlog/image/upload/c_scale,h_500/v1686222727/bd-ranking/iida_masanari_l20nh9.png",
     text: "孤高の破壊王",
     description: "元日本ウェルター10位",
   },
 
   {
     name: "瓜田純士",
-    src: "./urita_junshi.png",
+    src: "https://res.cloudinary.com/deevlog/image/upload/c_scale,h_500/v1686222998/bd-ranking/junshi_urita_oxnsyt.png",
     text: "アウトローのカリスマ",
     description: "",
   },
 
   {
     name: "バン仲村",
-    src: "./ban_nakamura.png",
+    src: "https://res.cloudinary.com/deevlog/image/upload/c_scale,h_500/v1686222866/bd-ranking/ban_nakamura_vkciuo.png",
     text: "あんたでもいいよ",
     description: "",
   },
 
   {
     name: "ぬりぼう",
-    src: "./nuribo.png",
+    src: "https://res.cloudinary.com/deevlog/image/upload/c_scale,h_500/v1686223019/bd-ranking/nuribo_owfjq2.png",
     text: "昇った太陽、二度と登らなくなりますよ。",
     description: "",
   },
 
   {
     name: "信原空",
-    src: "./nobuhara_sora.png",
+    src: "https://res.cloudinary.com/deevlog/image/upload/c_scale,h_500/v1686222969/bd-ranking/sora_nobuhara_afieb4.png",
     text: "トライフォースの孫悟空",
     description: "",
   },
 
   {
     name: "てる",
-    src: "./teru.png",
+    src: "https://res.cloudinary.com/deevlog/image/upload/c_scale,h_500/v1686222822/bd-ranking/teru_sa2mfu.png",
     text: "安保chの特攻隊長",
     description: "",
   },
 
   {
     name: "啓之輔",
-    src: "./keinosuke.png",
+    src: "https://res.cloudinary.com/deevlog/image/upload/c_scale,h_500/v1686223004/bd-ranking/keinosuke_gs8pz8.png",
     text: "キングオブ・アウトサイダー",
     description: "THE OUTSIDER 65-70kg級 初代/第４代チャンピオン",
   },
 
   {
     name: "１０人ニキ",
-    src: "./junin_niki.png",
+    src: "https://res.cloudinary.com/deevlog/image/upload/c_scale,h_500/v1686222793/bd-ranking/junin_niki_dacjed.png",
     text: "元祖最弱",
     description: "",
   },
 
   {
     name: "樋口武大",
-    src: "./higuchi_takehiro.png",
+    src: "https://res.cloudinary.com/deevlog/image/upload/c_scale,h_500/v1686222970/bd-ranking/takehiro_higuchi_q0spp7.png",
     text: "不屈のグラップラー",
     description: "THE OUTSIDER 60-65kg級 第2代/第４代チャンピオン",
   },
 
   {
     name: "こめお",
-    src: "./komeo.png",
+    src: "https://res.cloudinary.com/deevlog/image/upload/c_scale,h_500/v1686222909/bd-ranking/komeo_fahykj.png",
     text: "反逆の問題児",
     description: "",
   },
