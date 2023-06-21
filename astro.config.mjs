5151ae8751504a9d93c38f53c1553fc25c538a6e
Naoki <naoki.au.whm@gmail.com>
2023-06-21 21:13:30 +1000

    vercel added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), react()],
   site: "https://bd-ranking.vercel.app/",
+  output: "static",
+  adapter: vercel(),
 });
